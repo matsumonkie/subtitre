113416f3965c5ea5fad082676a1327760f79581b
Iori Matsuhara <matsuhar@gmail.com>
2017-07-02 18:24:52 +0200

    translate_to dropdown

--- hal/app/models/subtitle.rb
+++ hal/app/models/subtitle.rb
@@ -5,7 +5,12 @@ class Subtitle
   include ActiveModel::Model
 
   attr_accessor :file
-  enumerize :mode, in: [:easy, :normal, :hard], default: :easy, i18n_scope: "mode"
+  enumerize :mode, in: [:easy, :normal, :hard],
+            default: :easy,
+            i18n_scope: "mode"
+  enumerize :translate_to, in: [:french, :spanish],
+            default: :french,
+            i18n_scope: "translate_to"
 
   def with_temp_file
     begin
