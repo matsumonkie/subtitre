eda22c7791d466ed5cc5b5a399621c5ffa506cf8
Iori Matsuhara <matsuhar@gmail.com>
2017-07-06 15:47:07 +0200

    ++

--- hal/db/migrate/20170616150824_create_translations.rb
+++ hal/db/migrate/20170616150824_create_translations.rb
@@ -6,7 +6,6 @@ class CreateTranslations < ActiveRecord::Migration[5.1]
       t.string :site
       t.string :word
       t.json :response
-      t.string :translations, array: true
 
       t.timestamps
     end
