0a18004861e966b3b5d61693d9214d50ded3e0aa
Iori Matsuhara <matsuhar@gmail.com>
2017-07-11 10:05:49 +0200

    ++

--- hal/config/deploy.rb
+++ hal/config/deploy.rb
@@ -35,3 +35,9 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
