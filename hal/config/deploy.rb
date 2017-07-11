7c7d4c0f43f27e10455a937a20282e26256fd66f
Iori Matsuhara <matsuhar@gmail.com>
2017-07-11 12:19:01 +0200

    ++

--- hal/config/deploy.rb
+++ hal/config/deploy.rb
@@ -34,7 +34,7 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 #set :puma_access_log, "#{release_path}/log/puma.error.log"
 #set :puma_error_log,  "#{release_path}/log/puma.access.log"
 
-#set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"    #accept array for multi-bind
@@ -50,7 +50,7 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false
 set :puma_preload_app, false
-set :puma_daemonize, false
+set :puma_daemonize, true
 set :puma_tag, fetch(:application)
 
 # Default value for default_env is {}
