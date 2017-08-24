280678313021c2edf3d0bbfb5142748c468b8a7f
Iori Matsuhara <matsuhar@gmail.com>
2017-08-24 21:33:18 +0200

    ++

--- wall-e/src/AssetAsSet.hs
+++ wall-e/src/AssetAsSet.hs
@@ -10,14 +10,19 @@ import qualified Data.HashSet as HS
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Type
+import System.Directory
 
 assetPath :: FilePath -> FilePath
 assetPath file =
   "assets/" <> file
 
 assetAsSet :: FilePath -> IO TextSet
-assetAsSet file =
-  HS.fromList <$> T.lines <$> readAsset file
+assetAsSet file = do
+  fileExist <- doesFileExist $ assetPath file
+  if fileExist then
+    HS.fromList <$> T.lines <$> readAsset file
+  else
+    return HS.empty
   where
     readAsset :: FilePath -> IO T.Text
     readAsset file =
