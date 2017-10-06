d617ffa2cc5a56eb88d8469405a791015bc912c8
Iori Matsuhara <matsuhar@gmail.com>
2017-10-06 21:35:14 +0200

    ++

--- hal/app/controllers/subtitre_controller.rb
+++ hal/app/controllers/subtitre_controller.rb
@@ -5,6 +5,8 @@ class SubtitreController < ApplicationController
   SUBTITRE_TIMEOUT = 20
 
   def index
+    set_meta_tags title: 'Translate your subtitles',
+                  description: 'Translate your movie\'s subtitles. Translations are available from english and french to many languages.'
     @subtitle = Subtitle.new(translate_from: cookies[:prefered_orig_language],
                              translate_to:   cookies[:prefered_dest_language],
                              level:          cookies[:prefered_level])
