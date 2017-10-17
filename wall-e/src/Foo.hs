8eb08605bdc6a9858ab1d7e8fa0cb167eb182246
Iori Matsuhara <matsuhar@gmail.com>
2017-10-17 16:33:15 +0200

    ++

--- wall-e/src/Foo.hs
+++ wall-e/src/Foo.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DatatypeContexts #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TemplateHaskell #-}
 
+import Control.Lens
 import Common
 import Prelude()
 import Config.App
@@ -14,7 +18,7 @@ import Control.Monad.Trans.Class
 import Control.Monad.IO.Class
 
 import Control.Monad.State
-import Control.Monad.Writer
+--import Control.Monad.Writer
 import Control.Monad.Reader
 import qualified Data.Text as T
 import Control.Concurrent.Async
@@ -31,9 +35,31 @@ import Control.Concurrent
 import Redis.Channel
 import Redis.Connection
 import qualified Data.ByteString as BS
+import Data.Monoid
 
+data Family = Family { _father :: User
+                     , _mother :: User
+                     , _kids :: [User]
+                     } deriving Show
+
+data User = User { _name :: String
+                 , _job :: Maybe Job
+                 } deriving (Show)
+
+data Job = Writer | Actor | Teacher deriving Show
+
+initialFamily = Family { _father = User { _name = "Robert", _job = Just Writer }
+                       , _mother = User { _name = "Nadia", _job = Just Teacher }
+                       , _kids = [ User { _name = "Emily", _job = Just Actor }
+                                 , User { _name = "Nadia", _job = Nothing }
+                                 ]
+                       }
+
+makeLenses ''Family
+makeLenses ''User
+makeLenses ''Job
 
 main :: IO ()
 main = do
-  putStrLn $ show $
-    Common.foldl (\acc x -> acc + (T.length x)) 0 (["test"] :: [T.Text])
+--  putStrLn $ show initialFamily
+  putStrLn $ show $ ((initialFamily^.kids) !! 0) ^. name <> "test" -- = "test"
